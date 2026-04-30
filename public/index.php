b1b2239568df0b720025c81aa1af50aa6f892522
Luca N. <luca.notarfranco@gmail.com>
2026-04-30 12:02:33 +0200

    feat: creata la read per i contatti del db

--- public/index.php
+++ public/index.php
@@ -1,2 +1,31 @@
 <?php
+    // LOGICA PHP
     require_once "../config/database.php";
+
+    // Preparazione ed esecuzione query per mostrare dati di CONTATTI
+    try {
+        $stmt = $pdo->prepare("SELECT * FROM `contatti`");
+        $stmt->execute();
+        
+        $contatti = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e){
+        echo"Errore";
+    }
+?>
+
+    <!--HTML-->
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Rubrica</title>
+    </head>
+    <body>
+        <?php
+            foreach($contatti as $contatto) {
+                echo "{$contatto['nome']} {$contatto['cognome']}";
+            } 
+        ?>
+    </body>
+    </html>
