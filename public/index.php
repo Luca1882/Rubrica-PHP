f73d53a59cdd1044da47b3f3f65d988db169502d
Luca N. <luca.notarfranco@gmail.com>
2026-04-30 10:57:20 +0200

    feat: creata la connessione con il db tramite PDO e il file env

--- public/index.php
+++ public/index.php
@@ -1,11 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rubrica PHP</title>
-</head>
-<body>
-    <h1>CIAO MONDO !!!</h1>
-</body>
-</html>
+<?php
+    require_once "../config/database.php";
